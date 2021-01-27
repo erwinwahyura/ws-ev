5843f563b1e811a41e81c33eb106b297357ca6d2
erwinwahyura <erwinwahyuramadhan@gmail.com>
2021-01-27 19:08:36 +0700

    add: server http

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 3003 })
+const http = require('http')
+const server = http.createServer();
+const wss = new WebSocket.Server({ server })
 
 wss.on('connection', function conn(ws) {
   ws.on('message', function incoming(msg) {
     console.log('received: %s', msg)
     ws.send('Hello From Server', msg)
   })
+
+  // ws.on('apaya', function test(msg) {
+  //   console.log(msg, 'apaya')
+  // })
+  // wss.addListener('apaya', m => {
+  //   console.log('apaya : ', m)
+  // })
+})
+
+
+server.listen(3003, () => {
+  console.log('listening to port 3003')
 })
